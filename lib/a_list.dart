595095744d0cb4736da95c060cce04ec7cbbff80
yusriyahf <yusriyah9@gmail.com>
2022-11-20 13:45:41 +0700

    menyelesaikan semua test

--- lib/a_list.dart
+++ lib/a_list.dart
@@ -8,9 +8,11 @@ void main() {
 dynamic daftarMataKuliah() {
   // 1. Buat variable list yang menampung daftar mata kuliah
   // ...
-
+  List<String> mataKuliah = [];
+  mataKuliah.add("Daspro");
+  mataKuliah.add("Pancasila");
   // 2. returnkan variable tersebut
-  return null;
+  return mataKuliah;
 }
 
 // Nama function ini jangan diubah!
@@ -19,7 +21,8 @@ dynamic daftarIPSemester() {
   //    daftar nilai 8 IP semester (termasuk desimal)
   //    WAJIB 8 NILAI!!
   // ...
+  List<double> nilai = [80, 90, 70, 60, 70, 70, 80, 90];
 
   // 2. returnkan variable tersebut
-  return null;
+  return nilai;
 }
